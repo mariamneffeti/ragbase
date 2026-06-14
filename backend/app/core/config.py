57ae27dd5c7f5b6301bbe526df54e90f79b4dda4
thethrze <neffetimeriem@gmail.com>
2026-06-14 17:10:12 +0100

    added vercel url to allowed origins

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
 
     ALLOWED_ORIGINS: list[str] = [
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "https://ragbase-ten.vercel.app",
     ]
 
     PINECONE_API_KEY: SecretStr
